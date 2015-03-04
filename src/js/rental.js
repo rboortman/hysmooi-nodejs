2fee15e2ec9d0d7991deaa89a4c2c92dea7d5685
Ron Boortman <boortman@floodtags.com>
2015-03-04 20:24:49 +0100

    Added some links

---
+++ src/js/rental.js
@@ -0,0 +1,9 @@
+(function ($, window, undefined) {
+   $(document).ready(function() {
+      $('a.rental').fancybox({
+         'transitionIn': 'elastic',
+         'transitionOut': 'elastic',
+         'centerOnScroll': 'true'
+      });
+   });
+})(window.jQuery, window);
