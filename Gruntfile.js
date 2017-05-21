fe3755f6059e42cec70188c099c9e63cc8758d39
Ron Boortman <boortman@floodtags.com>
2017-05-21 17:17:15 +0200

    Adds support for internationalization

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,111 +1,111 @@
 module.exports = function(grunt) {
-   grunt.initConfig({
-      pkg: grunt.file.readJSON('package.json'),
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
 
-      watch: {
-         options: {
-            livereload: true
-         },
-         scsslint: {
-            files: ['<%= scsslint.files %>'],
-            tasks: [/*'scsslint',*/ 'sass']
-         },
-         jshint: {
-            files: ['<%= jshint.files %>', '.jshintrc'],
-            tasks: ['jshint', 'uglify']
-         },
-         jade: {
-            files: ['<%= sources.jade %>'],
-            tasks: ['copy:jade']
-         },
+    watch: {
+      options: {
+        livereload: true
       },
-      
       scsslint: {
-         files: ['<%= sources.scss %>'],
-         options: {
-            config: '.scss-lint.yml'
-         }
+        files: ['<%= scsslint.files %>'],
+        tasks: [/*'scsslint',*/ 'sass']
       },
-
       jshint: {
-         files: ['<%= sources.gruntfile %>', '<%= sources.js %>', 'index.js'],
-         options: {
-            jshintrc: '.jshintrc'
-         }
+        files: ['<%= jshint.files %>', '.jshintrc'],
+        tasks: ['jshint', 'uglify']
       },
+      pug: {
+        files: ['<%= sources.pug %>'],
+        tasks: ['copy:pug']
+      },
+    },
 
-      sass: {
-         options: {
-            loadPath: ['bower_components/bourbon/app/assets/stylesheets'],
-            style: 'compressed',
-            quiet: true,
-         },
-         dist: {
-            files: [{
-               expand: true,
-               cwd: 'src/scss',
-               src: ['*.scss'],
-               dest: 'build/css',
-               ext: '.css'
-            }]
-         }
-      },
-
-      uglify: {
-         options: {
-            mangle: {
-               toplevel: true
-            },
-            report: 'gzip'
-         },
-         files: {
-            src: ['<%= sources.js %>'],
-            dest: 'build/js',
-            expand: true,
-            flatten: true,
-            ext: '.min.js'
-         }
-      },
-      
-      copy: {
-         jade: {
-            expand: true,
-            cwd: 'src/jade',
-            src: '**',
-            dest: 'build/jade'
-         },
-         assets: {
-            expand: true,
-            cwd: 'assets/',
-            src: '**/*',
-            dest: 'build/',
-         },
-      },
-      
-      clean: {
-         data: ['build/*']
-      },
-
-      sources: {
-         jade: ['src/jade/**/*.jade'],
-         js: ['src/js/*.js', '!src/js/ga.js'],
-         scss: ['src/**/*.scss', '!src/scss/bourbon/**/*'],
-         build: ['build/**/*.js'],
-         gruntfile: ['Gruntfile.js']
+    scsslint: {
+      files: ['<%= sources.scss %>'],
+      options: {
+        config: '.scss-lint.yml'
       }
-   });
+    },
 
-   for (var dependency in grunt.config('pkg').dependencies) {
-      if (dependency.indexOf('grunt-') === 0) { grunt.loadNpmTasks(dependency); }
-   }
-   
-   if (process.env.NODE_ENV !== 'production') {
-      for (var devDependency in grunt.config('pkg').devDependencies) {
-         if (devDependency.indexOf('grunt-') === 0) { grunt.loadNpmTasks(devDependency); }
+    jshint: {
+      files: ['<%= sources.gruntfile %>', '<%= sources.js %>', 'index.js'],
+      options: {
+        jshintrc: '.jshintrc'
       }
-   }
+    },
 
-   grunt.registerTask('build', ['clean', 'sass', 'uglify', 'copy']);
-   grunt.registerTask('test', ['scsslint', 'jshint']);
-   grunt.registerTask('default', ['build']);
+    sass: {
+      options: {
+        loadPath: ['bower_components/bourbon/app/assets/stylesheets'],
+        style: 'compressed',
+        quiet: true,
+      },
+      dist: {
+        files: [{
+          expand: true,
+          cwd: 'src/scss',
+          src: ['*.scss'],
+          dest: 'build/css',
+          ext: '.css'
+        }]
+      }
+    },
+
+    uglify: {
+      options: {
+        mangle: {
+          toplevel: true
+        },
+        report: 'gzip'
+      },
+      files: {
+        src: ['<%= sources.js %>'],
+        dest: 'build/js',
+        expand: true,
+        flatten: true,
+        ext: '.min.js'
+      }
+    },
+
+    copy: {
+      pug: {
+        expand: true,
+        cwd: 'src/pug',
+        src: '**',
+        dest: 'build/pug'
+      },
+      assets: {
+        expand: true,
+        cwd: 'assets/',
+        src: '**/*',
+        dest: 'build/',
+      },
+    },
+
+    clean: {
+      data: ['build/*']
+    },
+
+    sources: {
+      pug: ['src/pug/**/*.pug'],
+      js: ['src/js/*.js', '!src/js/ga.js'],
+      scss: ['src/**/*.scss', '!src/scss/bourbon/**/*'],
+      build: ['build/**/*.js'],
+      gruntfile: ['Gruntfile.js']
+    }
+  });
+
+  for (var dependency in grunt.config('pkg').dependencies) {
+    if (dependency.indexOf('grunt-') === 0) { grunt.loadNpmTasks(dependency); }
+  }
+
+  if (process.env.NODE_ENV !== 'production') {
+    for (var devDependency in grunt.config('pkg').devDependencies) {
+      if (devDependency.indexOf('grunt-') === 0) { grunt.loadNpmTasks(devDependency); }
+    }
+  }
+
+  grunt.registerTask('build', ['clean', 'sass', 'uglify', 'copy']);
+  grunt.registerTask('test', ['scsslint', 'jshint']);
+  grunt.registerTask('default', ['build']);
 };
--- index.js
+++ index.js
@@ -11,65 +11,91 @@ var env = process.env.NODE_ENV || 'development';
 var db = JSON.parse(fs.readFileSync(__dirname + '/database.json', 'utf8'));
 
 switch (env) {
-   case 'development':
-      port = 5000;
-      break;
-      
-   case 'production':
-      port = 3000;
-      break;
+  case 'development':
+    port = 5000;
+    break;
+
+  case 'production':
+    port = 3000;
+    break;
 }
 
 var allowCrossDomain = function(req, res, next) {
-   res.header('Access-Control-Allow-Origin', req.header('Origin'));
-   res.header('Access-Control-Allow-Credentials', true);
-   res.header('Access-Control-Allow-Credentials', true);
-   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+  res.header('Access-Control-Allow-Origin', req.header('Origin'));
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
 
-   // intercept OPTIONS method
-   if ('OPTIONS' === req.method) {res.status(200).end();} else {next();}
+  // intercept OPTIONS method
+  if ('OPTIONS' === req.method) {res.status(200).end();} else {next();}
 };
 
 app.use(allowCrossDomain);
 app.use(express.static(contentPath));
 
-app.set('views', contentPath + '/jade');
-app.set('view engine', 'jade');
+app.set('views', contentPath + '/pug');
+app.set('view engine', 'pug');
 
 
 String.prototype.capitalize = function() {
-   var toReturn = [];
-   this.split('_').forEach(function (word) {
-      toReturn.push(word.charAt(0).toUpperCase() + word.slice(1));
-   });
-   return toReturn.join(' ');
+  var toReturn = [];
+  this.split('_').forEach(function (word) {
+    toReturn.push(word.charAt(0).toUpperCase() + word.slice(1));
+  });
+  return toReturn.join(' ');
 };
 
+app.get('/en/:page?', function (request, response) {
+  var toRender, toSend = {};
+
+  if (fs.existsSync(contentPath + '/pug/en/' + request.params.page + '.pug')) {
+    toSend.controller = request.params.page.capitalize();
+    toSend.development = true;
+    toRender = 'en/' + request.params.page;
+  } else {
+    toSend.controller = 'Home';
+    toSend.development = true;
+    toRender = 'en/home';
+  }
+
+  // response.send(request.params.page);
+  response.render(toRender, toSend);
+});
+
 app.get('/:page?', function (request, response) {
-   var toRender, toSend = {};
-   
-   if (fs.existsSync(contentPath + '/jade/' + request.params.page + '.jade')) {
-      toSend.controller = request.params.page.capitalize();
-      toSend.development = true;
-      toRender = request.params.page;
-   } else {
-      toSend.controller = 'Home';
-      toSend.development = true;
-      toRender = 'home';
-   }
-   
-   // response.send(request.params.page);
-   response.render(toRender, toSend);
+  var toRender, toSend = {};
+
+  if (fs.existsSync(contentPath + '/pug/' + request.params.page + '.pug')) {
+    toSend.controller = request.params.page.capitalize();
+    toSend.development = true;
+    toRender = request.params.page;
+  } else {
+    toSend.controller = 'Home';
+    toSend.development = true;
+    toRender = 'home';
+  }
+
+  // response.send(request.params.page);
+  response.render(toRender, toSend);
+});
+
+app.get('/en/projects/:id?', function (request, response) {
+  var toSend = {
+    controller: 'Projects',
+    project: db[request.params.id] || db.camphy,
+  };
+
+  response.render('en/projects', toSend);
 });
 
 app.get('/projects/:id?', function (request, response) {
-   var toRender, toSend = {
-      controller: 'Projects',
-      project: db[request.params.id] || db.camphy,
-   };
-   
-   response.render('projects', toSend);
+  var toSend = {
+    controller: 'Projects',
+    project: db[request.params.id] || db.camphy,
+  };
+
+  response.render('projects', toSend);
 });
 
 app.listen(port);
